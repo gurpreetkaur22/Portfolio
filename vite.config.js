7c48cde99a9354ad2b0088ea47c7d1bd81e779c7
Gurpreet Kaur <gurpreet.reehal29@gmail.com>
2025-12-31 18:01:18 +0530

    building to live 1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: '/Portfolio/', // Replace 'Portfolio' with your actual repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
